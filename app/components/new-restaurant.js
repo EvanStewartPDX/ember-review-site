06d33eacd82a6f69c67fbe0cfc8c652eee8fdfe7
evan <stewarea@gmail.com>
2016-11-04 09:53:58 -0700

    pushing to save. data disappeared, but seems to be back

--- app/components/new-restaurant.js
+++ app/components/new-restaurant.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+isShowingModal: false,
+ actions: {
+   toggleModal() {
+     this.toggleProperty('isShowingModal');
+   },
+   addRestaurant() {
+     var params = {
+       name: this.get('name') ? this.get('name') : "",
+       restaurant: this.get('restaurant') ? this.get('restaurant') : "",
+       zip: this.get('zip') ? this.get('zip') : "",
+       image: this.get('image') ? this.get('image') : "https://dummyimage.com/600x400/000/fff.png&text=??",
+       type: this.get('type') ? this.get('type') : "",
+     };
+   this.set('isShowingModal', false);
+   this.sendAction("addProduct", params);
+   this.set('name', "");
+   this.set('address', "");
+   this.set('zip', "");
+   this.set('image', "");
+   this.set('type', "");
+   }
+ }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     return this.store.findAll('restaurant');
+},
+actions: {
+  addRestaurant(params) {
+      var newRestaurant = this.store.createRecord('restaurant', params);
+      newRestaurant.save();
+      this.transitionTo('index');
+    }
   }
-});
+  });
