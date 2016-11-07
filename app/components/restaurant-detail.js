e9f2ff032edf6c67f61ce00d69576761215d7bce
Evan <stewarea@gmail.com>
2016-11-06 17:38:14 -0800

    add computed value fullAddress, some styling to jumbotron and favorites list

--- app/components/restaurant-detail.js
+++ app/components/restaurant-detail.js
@@ -1,22 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
-
+  fullAddress: Ember.computed('restaurant.address', 'restaurant.zip', function() {
+    return this.get('restaurant.address') + ', ' + this.get('restaurant.zip');
+  }),
   cart: Ember.inject.service('favorites-cart'),
+
   actions: {
   addToFavorites(restaurant) {
         this.get('cart').add(restaurant);
         console.log(restaurant);
-      },
-      showMap(restaurant) {
-        var container = this.$('.map-display')[0];
-        var options = {
-          center: this.get('map').center(restaurant.get('address'), restaurant.get('zip')),
-          zoom: 15
-
-        };
-        map = this.get('map').findMap(container, options);
       }
-  }
+    }
+
 });
