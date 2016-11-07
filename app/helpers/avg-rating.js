313ede83029a0d44487810f2ba5eccef13e938b5
Evan <stewarea@gmail.com>
2016-11-06 18:51:59 -0800

    small jumbotron styling, avg rating still doesnt' work

--- app/helpers/avg-rating.js
+++ app/helpers/avg-rating.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
 
 export function averageRating(params) {
-  // console.log(params);
-  // var sum = 0;
-  //
-  //
-  // for( var i = 0; i < review.ratings.length; i++ ){
-  //     sum += parseInt( ratings[i]);
-  //
-  // }
-  // var avg = sum/review.ratings.length;
-  // return avg;
-}
+//   console.log(params);
+//   var restaurant = params[0];
+//   var sum = 0;
+//
+//
+//   for( var i = 0; i < review.rating.length; i++ ){
+//       sum += parseInt( ratings[i]);
+//
+//   }
+//   var avg = sum/review.rating.length;
+//   return avg;
+// }
 export default Ember.Helper.helper(averageRating);
